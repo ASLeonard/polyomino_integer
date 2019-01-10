ec0974414949d3182866a799a1972f1a20f8438b
Alex Leonard <tkom343@gmail.com>
2019-01-10 16:20:05 +0000

    all

--- includes/integer_model.hpp
+++ includes/integer_model.hpp
@@ -6,10 +6,13 @@
 #include "core_evolution.hpp"
 
 
+using Genotype = std::vector<uint8_t>;
 
 class IntegerAssembly : public PolyominoAssembly<IntegerAssembly> {
-  
+  inline static uint8_t max_colour=8;
 public:  
-  static double InteractionMatrix(const int, const int);
+  static double InteractionMatrix(const uint8_t, const uint8_t);
+  inline static thread_local auto GenRandomSites = []() {return std::uniform_int_distribution<uint8_t>(0, max_colour)(RNG_Engine);};
+  static void Mutation(Genotype& genotype);
   
 };
--- src/integer_model.cpp
+++ src/integer_model.cpp
@@ -1,28 +1,33 @@
 #include "integer_model.hpp"
-thread_local std::mt19937 RNG_Engine(std::random_device{}());
 
 
-
-double IntegerAssembly::InteractionMatrix(const int face_1,const int face_2) {
+double IntegerAssembly::InteractionMatrix(const uint8_t face_1,const uint8_t face_2) {
   if(face_1==0 || face_2==0)
 	return 0;
   else
 	return (face_1+(face_1%2?1:-1))==face_2;
 }
+void IntegerAssembly::Mutation(Genotype& genotype) {
+  for(auto& base : genotype) {
+    const auto base_C=base;
+    do {
+      base=GenRandomSites();
+    }while(base==base_C);
+  }
+}
 
 int main() {
 
-std::cout<<"hi"<<std::endl;
 
-std::vector<int> g{0,0,0,0};//{0,0,1,0,2,2,2,2};
-    const std::vector<std::pair<InteractionPair,double> > edges = IntegerAssembly::GetActiveInterfaces(g);
+  std::vector<int> g{0,0,1,0, 2,2,2,2};
+  const std::vector<std::pair<InteractionPair,double> > edges = IntegerAssembly::GetActiveInterfaces(g);
 
-    std::vector<int8_t> assembly_information;
-    Phenotype phen;
-std::set<InteractionPair > interacting_indices;
-   assembly_information=IntegerAssembly::AssemblePolyomino(edges,interacting_indices);
-phen=GetPhenotypeFromGrid(assembly_information);
-PrintShape(phen);
+  std::vector<int8_t> assembly_information;
+  Phenotype phen;
+  std::set<InteractionPair > interacting_indices;
+  assembly_information=IntegerAssembly::AssemblePolyomino(edges,interacting_indices);
+  phen=GetPhenotypeFromGrid(assembly_information);
+  std::cout<<phen<<std::endl;
 
 }
 
